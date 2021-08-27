5deddd7c9c20e0056814d26482ac39cfca8cb0de
Himmel König <itogreniyorum@gmail.com>
2021-08-28 01:31:45 +0300

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,25 @@ import React, { useState } from 'react';
 
 
 function NavBar() {
-
-
-
     return(
         <div>
-                <nav>
-                    <div id='nav-links' >
-                        <a href='/'>Home</a>
-                        <a href='/about'>About</a>
-                        <a href='/blog'>Blog</a>
-                        <a href='/contact'>Contact</a>
-                    </div>
-                </nav>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+		  <a class="navbar-brand text-danger" href="#">ApiaryBee</a>
+		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav ms-auto">
+		      <a class="nav-link active" aria-current="page" href="#">Home</a>
+		      <a class="nav-link"  href="#">About Us</a>
+		      <a class="nav-link" href="#">Services</a>
+		      <a class="nav-link" href="#">Support</a>
+		      <a class="nav-link" href=".#">Contact</a>
+		    </div>
+		  </div>
+		</div>
+		</nav>
             </div>
         );
 }
